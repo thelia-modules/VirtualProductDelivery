7e615783473b58aa3d8db380b81f45579747296a
Alexandre Nozière <anoziere@openstudio.fr>
2026-09-16 10:57:35 +0200

    fix(mail): write the message subjects in Twig

--- I18n/tr_TR.php
+++ I18n/tr_TR.php
@@ -11,7 +11,7 @@
  */
 
 return [
-    'Order {$order_ref} validated. Download your files.' => 'Doğrulanmış {$order_ref} sipariş. Download senin eğe.',
+    'Order {{ order_ref }} validated. Download your files.' => 'Doğrulanmış {{ order_ref }} sipariş. Download senin eğe.',
     'The file [%file] does not exist' => '[%file] dosyası yok',
     'This module cannot be used on the current cart.' => 'Bu modül geçerli arabaya kullanılamaz.',
     'Virtual product download message' => 'Sanal ürün indir mesaj',
